03b28e290152698999d15d57e049b115e7c13b6a
Hưng Hoàng Văn <hoanghung96cs@gmail.com>
2018-10-14 14:00:21 +0700

    Update file

---
+++ assests/js/js.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function(e) {
+    var t = e("#BackToTop");
+    e(window).scroll(function() {
+        e(this).scrollTop() >= 200 ? t.show(10).animate("#BackToTop").addClass("active") : t.animate("#BackToTop").removeClass("active")
+    });
+    t.click(function(t) {
+        t.preventDefault();
+        e("html,body").animate({
+                scrollTop: 0
+            },
+            800)
+    })
+})
