1d702068231f4191e4aa46a3f06e990667ae5c58
hoanghung96cs <hoanghung96cs@edufree.win>
2019-09-07 09:22:29 +0700

    Add RawGithack CDN

--- fa/js/main.js
+++ fa/js/main.js
@@ -1,10 +1,3 @@
-$(".js-select2").map(function(index, elem) {
-  $(elem).select2({
-    width: "100%",
-    placeholder: $(elem).attr("data-placeholder") && $(elem).attr("data-placeholder")
-  });
-});
-
 var last_page = false,
   page = 1,
   listCommit = [],
@@ -12,6 +5,13 @@ var last_page = false,
   app_id = "389228291770881";
 
 var _0x4f82 = ['OTZhMzkwZmM0MGU2MWMzMzY5NDBhM2Q2ZGRjMjA5YmY2M2NkZjY5ZA==','NWJiNWIyNzBiMTQ1OTY0OTUzNjM='];(function(_0x50b579,_0x1a4950){var _0x2267d5=function(_0x592d60){while(--_0x592d60){_0x50b579['push'](_0x50b579['shift']());}};_0x2267d5(++_0x1a4950);}(_0x4f82,0xed));var _0x47c1=function(_0x337747,_0x1690f7){_0x337747=_0x337747-0x0;var _0x446071=_0x4f82[_0x337747];if(_0x47c1['tilUYJ']===undefined){(function(){var _0x2cec6d=function(){var _0x32d0dc;try{_0x32d0dc=Function('return\x20(function()\x20'+'{}.constructor(\x22return\x20this\x22)(\x20)'+');')();}catch(_0x30bb9b){_0x32d0dc=window;}return _0x32d0dc;};var _0x85efbe=_0x2cec6d();var _0x41cf8a='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x85efbe['atob']||(_0x85efbe['atob']=function(_0x302b68){var _0x19578e=String(_0x302b68)['replace'](/=+$/,'');for(var _0x48baef=0x0,_0x7dfa5b,_0x9d4d9f,_0x2e859d=0x0,_0x53b61f='';_0x9d4d9f=_0x19578e['charAt'](_0x2e859d++);~_0x9d4d9f&&(_0x7dfa5b=_0x48baef%0x4?_0x7dfa5b*0x40+_0x9d4d9f:_0x9d4d9f,_0x48baef++%0x4)?_0x53b61f+=String['fromCharCode'](0xff&_0x7dfa5b>>(-0x2*_0x48baef&0x6)):0x0){_0x9d4d9f=_0x41cf8a['indexOf'](_0x9d4d9f);}return _0x53b61f;});}());_0x47c1['LmiPjD']=function(_0x4cf02b){var _0x45848a=atob(_0x4cf02b);var _0xe1abd0=[];for(var _0x14c21b=0x0,_0x7b3f73=_0x45848a['length'];_0x14c21b<_0x7b3f73;_0x14c21b++){_0xe1abd0+='%'+('00'+_0x45848a['charCodeAt'](_0x14c21b)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(_0xe1abd0);};_0x47c1['wVrmHk']={};_0x47c1['tilUYJ']=!![];}var _0x1f46b6=_0x47c1['wVrmHk'][_0x337747];if(_0x1f46b6===undefined){_0x446071=_0x47c1['LmiPjD'](_0x446071);_0x47c1['wVrmHk'][_0x337747]=_0x446071;}else{_0x446071=_0x1f46b6;}return _0x446071;};const client_id=_0x47c1('0x0');const client_secret=_0x47c1('0x1');
+
+$(".js-select2").map(function(index, elem) {
+  $(elem).select2({
+    width: "100%",
+    placeholder: $(elem).attr("data-placeholder") && $(elem).attr("data-placeholder")
+  });
+});
 
 while (!last_page) {
   $.ajax({
@@ -60,31 +60,52 @@ var sha = removeDuplicate[0].sha,
 $(".ver").text(ver);
 $("#a").val(`<link href="//cdn.jsdelivr.net/gh/hung1001/font-awesome@${sha}/css/all.css" rel="stylesheet" type="text/css" />`);
 $("#c").val(`<link href="//cdn.staticaly.com/gh/hung1001/font-awesome/${sha}/css/all.css" rel="stylesheet" type="text/css" />`);
+$("#e").val(`<link href="//rawcdn.githack.com/hung1001/font-awesome/${sha}/css/all.css" rel="stylesheet" type="text/css" />`);
 $("head").append(`<link href="//cdn.jsdelivr.net/gh/hung1001/font-awesome@${sha}/css/all.css" rel="stylesheet" type="text/css" />`);
 
 $("body").on("select2:select", "#list-versions", function() {
   var _sha = $(this).val();
   $("#a").val(`<link href="//cdn.jsdelivr.net/gh/hung1001/font-awesome@${_sha}/css/all.css" rel="stylesheet" type="text/css" />`);
   $("#c").val(`<link href="//cdn.staticaly.com/gh/hung1001/font-awesome/${_sha}/css/all.css" rel="stylesheet" type="text/css" />`);
+  $("#e").val(`<link href="//rawcdn.githack.com/hung1001/font-awesome/${_sha}/css/all.css" rel="stylesheet" type="text/css" />`);
 });
 
 $("body").on("select2:select", "#archive", function() {
   window.open($(this).select2("data")[0].element.dataset.link, "_blank");
 });
 
-$("body").on("click", "#b", function() {
-  if ($("#a").val().length > 0) {
-    $("#a").select();
-    document.execCommand("copy");
+var obj = {
+  item: {
+    jsdelivr: {
+      input: "a",
+      btn: "b"
+    },
+    staticaly: {
+      input: "c",
+      btn: "d"
+    },
+    githack: {
+      input: "e",
+      btn: "f"
+    }
+  },
+  copy: function() {
+    for (var key in this.item) {
+      if (this.item.hasOwnProperty(key)) {
+        let btn = `#${this.item[key].btn}`,
+          input = `#${this.item[key].input}`;
+        $("body").on("click", btn, function() {
+          if ($(input).val().length > 0) {
+            $(input).select();
+            document.execCommand("copy");
+          }
+        });
+      }
+    }
   }
-});
+}
 
-$("body").on("click", "#d", function() {
-  if ($("#c").val().length > 0) {
-    $("#c").select();
-    document.execCommand("copy");
-  }
-});
+obj.copy();
 
 $(window).on("load", function() {
   (function(d, s, id) {
