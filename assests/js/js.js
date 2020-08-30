3f7bedd8fcd2689ac73c2c6529f928c01a71eb82
hung1001 <hoanghung96cs@gmail.com>
2020-08-30 09:25:53 +0700

    Update header, remove outdate CDN

--- assests/js/js.js
+++ assests/js/js.js
@@ -1,13 +1,14 @@
 jQuery(document).ready(function(e) {
-    var t = e("#BackToTop");
-    e(window).scroll(function() {
-        e(this).scrollTop() >= 200 ? t.show(10).animate("#BackToTop").addClass("active") : t.animate("#BackToTop").removeClass("active")
-    });
-    t.click(function(t) {
-        t.preventDefault();
-        e("html,body").animate({
-                scrollTop: 0
-            },
-            800)
-    })
-})
+  var t = e('#BackToTop');
+
+  e(window).scroll(function() {
+    e(this).scrollTop() >= 200 ? t.show(10).animate('#BackToTop').addClass('active') : t.animate('#BackToTop').removeClass('active');
+  });
+
+  t.click(function(t) {
+    t.preventDefault();
+    e('html,body').animate({
+      scrollTop: 0
+    }, 800);
+  });
+});
